272e77f8a039c51aebfa9735cfe14991e55f410d
william <willydark56@gmail.com>
2025-02-12 12:35:52 -0500

    init with controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req, Res } from '@nestjs/common';
+import { Response, Request } from 'express';
+import { AuthService } from './auth.service';
 
-@Controller({})
+@Controller('auth')
 export class AuthController {
 
-    @Get('/auth')
-    getAllUsers(){
-        return 'Probando end point'
+
+    constructor(private authService: AuthService){}
+
+    @Get()
+    getAllUsers(@Req() request: Request, @Res() response: Response){
+        return response.json({msg: this.authService.getUsers(), state: 'ok'}).status(200)
+    }
+    @Post('/:id')
+    createUser(@Param('id') param: string){
+        console.log(param)
     }
 }
